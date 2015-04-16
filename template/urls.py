8b14d88f1ae1e1f897a0773e946e80377894453f
redbrick9 <redbrick9@gmail.com>
2015-04-16 17:43:40 +0800

    Adding launch image, list of image and vm.

--- template/urls.py
+++ template/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<template_name>[0-9a-zA-Z_.-]+)/launch/$', views.launch, name='launch'),
+    url(r'^vm/$', views.vm_list, name='vm_list'),
+    url(r'^vm/new/$', views.vm_new, name='vm_new'),
 ]
--- template/views.py
+++ template/views.py
@@ -8,7 +8,6 @@ from pyVmomi import vim, vmodl
 import atexit
 
 def index(request):
-
     # Get ServiceInstanceContent
     si = SmartConnect(host='172.20.10.2',
                       user='root',
@@ -37,5 +36,88 @@ def index(request):
     })
 
     template = loader.get_template('template/index.html')
+    return HttpResponse(template.render(context))
+
+def launch(request, template_name):
+    context = RequestContext(request, {
+        'image': template_name
+    })
+    template = loader.get_template('template/launch.html')
+    return HttpResponse(template.render(context))
+
+def vm_new(request):
+    image = request.POST['image']
+    vmName = request.POST['name']
+
+    # Get ServiceInstanceContent
+    si = SmartConnect(host='172.20.10.2',
+                      user='root',
+                      pwd='vmware')
+    if not si:
+        return HttpResponse("Hi, guys. I'm sorry for that we can't connect to vcenter.")
+    atexit.register(Disconnect, si)
+    content = si.RetrieveContent()
+
+    objView = content.viewManager.CreateContainerView(content.rootFolder,
+                                                      [vim.VirtualMachine],
+                                                      True)
+    vmList = objView.view
+    objView.Destroy()
+
+    # Get all vms under the specific '__vapor_vms'
+    objView = content.viewManager.CreateContainerView(content.rootFolder,
+                                                      [vim.Folder],
+                                                      True)
+    folderList = objView.view
+    objView.Destroy()
+    
+    def clone(vm):
+       def cloneto(folder):
+          print("Clone VM %s to folder %s" % (vm, folder))
+
+          relocateSpec = vim.vm.RelocateSpec()
+
+          cloneSpec = vim.vm.CloneSpec()
+          cloneSpec.location = relocateSpec
+
+          return vm.Clone(folder, vmName, cloneSpec)
+
+       return [cloneto(folder) for folder in folderList if folder.name == '__vapor_vms']
+
+    tasks = [clone(vm) for vm in vmList if vm.name == image]
+
+    context = RequestContext(request, {
+    })
+    template = loader.get_template('template/vm_new.html')
+    return HttpResponse(template.render(context))
+
+def vm_list(request):
+    # Get ServiceInstanceContent
+    si = SmartConnect(host='172.20.10.2',
+                      user='root',
+                      pwd='vmware')
+    if not si:
+        return HttpResponse("Hi, guys. I'm sorry for that we can't connect to vcenter.")
+    atexit.register(Disconnect, si)
+    content = si.RetrieveContent()
+
+    # Get all vms under the specific '__vapor_vms'
+    vms = []
+    objView = content.viewManager.CreateContainerView(content.rootFolder,
+                                                      [vim.Folder],
+                                                      True)
+    folderList = objView.view
+    for folder in folderList:
+       if folder.name == '__vapor_vms':
+          vmList = folder.childEntity
+          vms.extend(vmList)
+          break
+       else:
+          continue 
+ 
+    context = RequestContext(request, {
+        'vms': vms
+    })
+    template = loader.get_template('template/vm_list.html')
     return HttpResponse(template.render(context))
 
